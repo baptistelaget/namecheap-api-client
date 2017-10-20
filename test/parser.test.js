d95147344e4ddf86fa87c699320ae797bb1617e7
Antoine Beauvais-Lacasse <abeaulac@octhuber.com>
2017-10-20 17:54:15 -0400

    Separate proxy from params.

---
+++ test/parser.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const client = require('./test.client');
+
+client.executeCommand('namecheap.domains.getInfo', {DomainName: 'www.namecheap.com'})
+      .then(result => console.log(result))
+      .catch(err => console.error('got error'));
