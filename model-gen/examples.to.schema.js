0792176b805bb817843aa3155540dfcb5d391831
Antoine Beauvais-Lacasse <abeaulac@octhuber.com>
2017-10-20 19:42:43 -0400

    Use samples in schema generation.

--- model-gen/examples.to.schema.js
+++ model-gen/examples.to.schema.js
@@ -1,25 +1,34 @@
 'use strict';
 global.Promise = Promise = require('bluebird');
-const execFile = Promise.promisify(require('child_process').execFile);
 const path = require('path');
 const fs = require('fs');
+const _ = require('lodash');
+const execFile = Promise.promisify(require('child_process').execFile);
+const readdir = Promise.promisify(fs.readdir);
 
 
-const schemaPath = path.resolve(__dirname, 'schema', 'namecheap.api.xsd');
+const schemaPath = path.resolve(__dirname, 'schema', 'namecheap.api_sampled.xsd');
+
+// TODO RECOMPILE TRANG TO USE CONTENT INSTEAD OF MIXED TYPES.
 const trangPath = path.resolve(__dirname, 'trang-20091111.jar');
 
 
-function generateSchemaFromExamples(examplesDir) {
-    const exampleFiles = fs.readdirSync(examplesDir)
-                           .filter(file => file.endsWith('.xml'))
-                           .map(file => path.join(examplesDir, file));
-
-    return execFile(
-        'java',
-        ['-jar', trangPath, ...exampleFiles, schemaPath],
-        {cwd: __dirname}
-    ).return(schemaPath);
+function _findExamplesInDir(dir) {
+    return readdir(dir).then(files => files.filter(file => file.endsWith('.xml'))
+                                           .map(file => path.join(dir, file)));
 }
 
+function _enumerateFiles(...examplesDirs) {
+    return Promise.map(examplesDirs, _findExamplesInDir)
+                  .then(files => _.flattenDeep(files));
+}
+
+function generateSchemaFromExamples(...examplesDirs) {
+    return _enumerateFiles(...examplesDirs)
+        .then(exampleFiles => execFile('java', ['-jar', trangPath, ...exampleFiles, schemaPath], {cwd: __dirname}))
+        .return(schemaPath);
+}
 
 module.exports = {generateSchemaFromExamples};
+
+generateSchemaFromExamples('./samples', './examples');
